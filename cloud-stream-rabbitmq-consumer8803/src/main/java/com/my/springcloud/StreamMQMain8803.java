82da9ffae91ffc7ae1611fcdd4b43962fa4f3375
chenshan <chenshan@qq.com>
2020-05-22 10:54:05 +0800

    spring-cloud-stream 分组消费与持久化

---
+++ cloud-stream-rabbitmq-consumer8803/src/main/java/com/my/springcloud/StreamMQMain8803.java
@@ -0,0 +1,15 @@
+package com.my.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author chenshan
+ * @date 2020-05-22 9:33
+ */
+@SpringBootApplication
+public class StreamMQMain8803 {
+  public static void main(String[] args) {
+      SpringApplication.run(StreamMQMain8803.class,args);
+    }
+}
